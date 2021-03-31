57ac57a20d403a468e3e2569107034a773260d2e
Zubair Bester <Zubair.bester120390@gmail.com>
2021-03-31 15:20:10 +0200

    Ending till seeding data part two

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -19,16 +19,13 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
           
         }
-
-
-        [Authorize]
-         [HttpGet("{id}")]
+        
+        [HttpGet("{id}")]
         public async  Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
