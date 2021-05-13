1d076924b2d9ababa556e12eb4c86964dde14945
Zubair Bester <Zubair.bester120390@gmail.com>
2021-05-13 23:14:59 +0200

    End Of Section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,12 +1,12 @@
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using API.Data;
-using API.DTOs;
-using API.Entities;
-using API.Interfaces;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+    using API.Data;
+    using API.DTOs;
+    using API.Entities;
+    using API.Interfaces;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
